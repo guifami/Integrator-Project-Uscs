559ae23f9c4171bca102ef515969136408b64744
guifami <93688391+guifami@users.noreply.github.com>
2021-11-08 09:57:17 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,3 +24,21 @@ function colorLink(){
 }
 
 linkColor.forEach(l => l.addEventListener('click', colorLink))
+
+
+
+/*==================== Flex Box Cards Content ====================*/
+images = [
+    "https://source.unsplash.com/featured/?mountains",
+    "https://source.unsplash.com/featured/?hiking",
+    "https://source.unsplash.com/featured/?kayak",
+    "https://source.unsplash.com/featured/?forest",
+    "https://source.unsplash.com/featured/?mountain"
+  "https://source.unsplash.com/featured/?trail"
+  "https://source.unsplash.com/featured/?outdoors"
+"https://source.unsplash.com/featured/?norway"
+]
+
+$('div.card-image').each ->
+    random_image_index = Math.floor(images.length * Math.random())
+$(this).css('background-image', 'url(' + images[random_image_index] + ')')
